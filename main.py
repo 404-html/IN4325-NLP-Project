b8b129e9120c98293d995a0ad901193f9b7eac3f
albertoviguria <alberto.viguria@gmail.com>
2019-04-10 16:46:30 +0200

    reduced features

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from metric_labeling import metric_labeling, train_nnc
 from polarity_feature import make_polarity_features
 from util import Author
 from scipy.sparse import csr_matrix
+import pandas as pd
 
 # Load preprocessed data.
 data, sentences = get_data(Author.SCHWARTZ)
@@ -27,7 +28,7 @@ data_train, data_test, labels_train, labels_test = train_test_split(sentences, y
                                                                     test_size=0.20,
                                                                     random_state=42)
 
-vectorizer_tf = TfidfVectorizer(max_features=12000)
+vectorizer_tf = TfidfVectorizer(max_features=8000)
 # X_tf_train = vectorizer_tf.fit_transform(data_train)
 # X_tf_test = vectorizer_tf.transform(data_test)
 
@@ -47,18 +48,18 @@ X_combined_test = X_combined_test.transform(data_test).todense()
 PSP_array_train, last_sentence_sentiment_array_train, first_sentence_sentiment_array_train = make_polarity_features(data_train)
 PSP_array_test, last_sentence_sentiment_array_test, first_sentence_sentiment_array_test = make_polarity_features(data_test)
 
-# # Append these features to the original feature matrix
-# X_combined_train = np.hstack((X_combined_train, np.asmatrix(PSP_array_train)))
-# X_combined_train = np.hstack(
-#     (X_combined_train, np.asmatrix(last_sentence_sentiment_array_train)))
-# #X_combined_train = np.hstack(
-# #    (X_combined_train, np.asmatrix(first_sentence_sentiment_array_train)))
-#
-# X_combined_test = np.hstack((X_combined_test, np.asmatrix(PSP_array_test)))
-# X_combined_test = np.hstack(
-#     (X_combined_test, np.asmatrix(last_sentence_sentiment_array_test)))
-# #X_combined_test = np.hstack(
-# #    (X_combined_test, np.asmatrix(first_sentence_sentiment_array_test)))
+# Append these features to the original feature matrix
+X_combined_train = np.hstack((X_combined_train, np.asmatrix(PSP_array_train)))
+X_combined_train = np.hstack(
+    (X_combined_train, np.asmatrix(last_sentence_sentiment_array_train)))
+X_combined_train = np.hstack(
+    (X_combined_train, np.asmatrix(first_sentence_sentiment_array_train)))
+
+X_combined_test = np.hstack((X_combined_test, np.asmatrix(PSP_array_test)))
+X_combined_test = np.hstack(
+    (X_combined_test, np.asmatrix(last_sentence_sentiment_array_test)))
+X_combined_test = np.hstack(
+    (X_combined_test, np.asmatrix(first_sentence_sentiment_array_test)))
 
 y = data['class'].values
 print("\nClass values: ")
@@ -70,21 +71,23 @@ print(X_combined_train)
 X_combined_train = csr_matrix(X_combined_train)
 X_combined_test = csr_matrix(X_combined_test)
 
-# hyperparameter selection
-param_grid = {'gamma': np.linspace(0.2, 0.6, 4), 'C': np.linspace(2.8, 3.2, 4)}
+# hyperparameter selection rbf ovo
+param_grid = {'gamma': [0.1, 0.18, 0.2, 0.25], 'C': np.linspace(3.8, 4.2, 4)}
 model = svm.SVC()
 clf = model_selection.GridSearchCV(model, param_grid, cv=10, return_train_score=True)
 clf.fit(X_combined_train, labels_train)
 print (clf.best_params_)
 print (clf.best_score_)
-#Best parameters {'C': 2.8, 'gamma': 0.33333333333333337}
+search_results = pd.DataFrame.from_dict(clf.cv_results_)
+print(search_results.loc[:, ['param_gamma', 'param_C', 'mean_test_score']])
+#Best parameters {'C': 3.93, 'gamma': 0.18}
 
 # SVC OVO
-clf = svm.SVC(gamma=0.2, C=3.2, decision_function_shape='ovo')
-clf.fit(X_combined_train, labels_train)
+#clf = svm.SVC(gamma=0.2, C=3.2, decision_function_shape='ovo')
+#clf.fit(X_combined_train, labels_train)
 y_predicted = clf.predict(X_combined_test)
 print(accuracy_score(labels_test, y_predicted))
-plot_confusion_matrix(labels_test, y_predicted, np.array(('0', '1', '2')))
+plot_confusion_matrix(labels_test, y_predicted, np.array(('0', '1', '2')), normalize=True)
 plt.show()
 
 # Metric labeling
